55cbd498c6f96f7905b712577df4d1b73bd4caf9
TrasherDK <trasherdk@fumlersoft.dk>
2022-10-12 07:26:06 +0200

    Add unique index on name

--- prisma/migrations/20221012052200_init/migration.sql
+++ prisma/migrations/20221012052200_init/migration.sql
@@ -6,5 +6,6 @@ CREATE TABLE `player` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
+    UNIQUE INDEX `player_name_key`(`name`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
